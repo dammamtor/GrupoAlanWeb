3d211b956fb11a41e2d7a5450f04eb49674af4ca
judasmclovin <judasmclovin@vivaldi.net>
2024-04-10 07:30:38 +0200

    creacion de gAlan backend

---
+++ backendGalan/src/main/java/grupoalan/backendgalan/BackendGalanApplication.java
@@ -0,0 +1,13 @@
+package grupoalan.backendgalan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGalanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGalanApplication.class, args);
+	}
+
+}
---
+++ backendGalan/src/test/java/grupoalan/backendgalan/BackendGalanApplicationTests.java
@@ -0,0 +1,13 @@
+package grupoalan.backendgalan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGalanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
