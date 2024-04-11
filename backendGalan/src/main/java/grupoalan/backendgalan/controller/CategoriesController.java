44bb71e63b063df4681d00731165de6eb114f406
judasmclovin <judasmclovin@vivaldi.net>
2024-04-11 11:15:11 +0200

    ya recoge las peticiones, solo falta devolverlas

--- backendGalan/src/main/java/grupoalan/backendgalan/controller/CategoriesController.java
+++ backendGalan/src/main/java/grupoalan/backendgalan/controller/CategoriesController.java
@@ -7,16 +7,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @RestController
 public class CategoriesController {
+    static final Logger logger = LoggerFactory.getLogger(CategoriesController.class);
+
     @Autowired
     private CategoriesService categoriesService;
 
     @GetMapping("/sync/categories")
     public ResponseEntity<List<Categories>> syncCategories() {
+        logger.info("HAZ HECHO UNA PETICION A LISTA DE CATEGORIAS");
         List<Categories> categories = categoriesService.syncCategoriesFromApi();
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
--- backendGalan/src/main/java/grupoalan/backendgalan/services/CategoriesService.java
+++ backendGalan/src/main/java/grupoalan/backendgalan/services/CategoriesService.java
@@ -1,48 +1,62 @@
 package grupoalan.backendgalan.services;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import grupoalan.backendgalan.controller.CategoriesController;
 import grupoalan.backendgalan.model.Categories;
 import grupoalan.backendgalan.model.response.CategoryResponse;
 import grupoalan.backendgalan.repository.CategoriesRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
 
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class CategoriesService {
+    static final Logger logger = LoggerFactory.getLogger(CategoriesService.class);
     @Autowired
     private RestTemplate restTemplate;
 
     @Value("${api.token}")
     private String apiToken;
+    private final ObjectMapper objectMapper;
 
     @Autowired
     private CategoriesRepository categoriesRepository;
 
     private static final String API_URL = "https://data.makito.es/api/categories";
 
+    public CategoriesService(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     public List<Categories> syncCategoriesFromApi() {
+        logger.info("ESTAS EN EL CATEGORIES SERVICE");
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + apiToken);
 
-        // Realizar la llamada a la API externa
-        CategoryResponse[] responses = restTemplate.getForObject(API_URL, CategoryResponse[].class);
+        logger.info("apiToken: " + apiToken);
 
-        // Procesar las respuestas y guardarlas en la base de datos
-        List<Categories> categoriesList = new ArrayList<>();
-        for (CategoryResponse response : responses) {
-            Categories category = new Categories();
-            category.setName(response.getCategory());
-            // Agregar la categoría a la lista
-            categoriesList.add(category);
-        }
-        // Guardar todas las categorías en la base de datos
-        return categoriesRepository.saveAll(categoriesList);
+        HttpEntity<Void> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                API_URL, HttpMethod.GET, requestEntity, String.class, (Object) null);
+
+        logger.info("response: " + response.getBody());
+        
+        return null;
     }
 
 
